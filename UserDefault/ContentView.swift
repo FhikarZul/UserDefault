9d2e81b6004a5db29dea37b0989a4f7482ab9339
Fhikar Zul <Fhikarzul18@gmail.com>
2022-08-29 15:22:11 +0800

    Initial Commit

---
+++ UserDefault/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDefault
+//
+//  Created by Lucy on 29/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserDefault/UserDefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultApp.swift
+//  UserDefault
+//
+//  Created by Lucy on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
